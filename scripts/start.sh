69e14685aa99e54196f2b1ec7227d44ab7c84225
Abhi Thube <athube@abhithube.com>
2025-12-13 13:47:38 -0800

    Initial commit

---
+++ scripts/start.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+
+ROOT_DIR=/workspace
+COMFYUI_DIR=$ROOT_DIR/ComfyUI
+
+CUSTOM_NODES=(
+  "https://github.com/Comfy-Org/ComfyUI-Manager"
+  "https://github.com/city96/ComfyUI-GGUF"
+)
+
+cd $ROOT_DIR
+
+echo "Installing ComfyUI..."
+git clone https://github.com/comfyanonymous/ComfyUI
+
+cd $COMFYUI_DIR
+
+echo "Installing ComfyUI dependencies..."
+pip install -r requirements.txt
+
+for repo in ${CUSTOM_NODES[@]}; do
+  repo_name=$(basename $repo)
+
+  if [ ! -d $COMFYUI_DIR/custom_nodes/$repo_name ]; then
+    cd $COMFYUI_DIR/custom_nodes
+
+    echo "Installing $repo_name..."
+    git clone $repo
+  else
+    cd $COMFYUI_DIR/custom_nodes/$repo_name
+
+    echo "Updating $repo_name..."
+    git pull
+  fi
+
+  if [ ! -d $COMFYUI_DIR/custom_nodes/$repo_name/requirements.txt ]; then
+    cd $COMFYUI_DIR/custom_nodes/$repo_name
+
+    echo "Updating $repo_name dependencies..."
+    pip install -r requirements.txt
+  fi
+done
+
+cd $COMFYUI_DIR
+
+cat > extra_model_paths.yaml << 'EOF'
+comfyui:
+    base_path: /opt/comfyui/models/
+    is_default: true
+    checkpoints: checkpoints
+    text_encoders: text_encoders
+    clip_vision: clip_vision
+    configs: configs
+    controlnet: controlnet
+    diffusion_models: |
+      diffusion_models
+      unet
+    embeddings: embeddings
+    loras: loras
+    upscale_models: upscale_models
+    vae: vae
+    audio_encoders: audio_encoders
+    model_patches: model_patches
+EOF
+
+echo "Starting ComfyUI server..."
+python main.py --listen 0.0.0.0 --port 8188
