cba325b3934ff97e3aa2e986389675633824d1bf
Abhi Thube <athube@abhithube.com>
2026-07-30 00:18:12 -0700

    Removed Z-Image LoRAs

--- scripts/z-image.sh
+++ scripts/z-image.sh
@@ -8,9 +8,6 @@ uvx hf download --local-dir . Comfy-Org/z_image_turbo \
   split_files/text_encoders/qwen_3_4b.safetensors \
   split_files/vae/ae.safetensors
 
-uvx hf download --local-dir $MODELS_DIR/loras Danrisi/GrainScape_UltraReal_ZImage grainscape_zimage.safetensors
-uvx hf download --local-dir $MODELS_DIR/loras Danrisi/Lenovo_UltraReal_Z_Image lenovo_z.safetensors
-
 mkdir -p $MODELS_DIR/diffusion_models
 mv split_files/diffusion_models/z_image_turbo_bf16.safetensors $MODELS_DIR/diffusion_models
 
